5e49c4bd89af30f4971a236e3e3e5327d27fc1b6
Aditya Ramaakrushnan <48169134+adiram359@users.noreply.github.com>
2020-06-12 21:47:30 -0700

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -2,19 +2,25 @@ const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const sortButton = document.querySelector('.algobutton')
+console.log(sortButton)
+sortButton.addEventListener('click', () => sort())
+
+
+
 var c = canvas.getContext('2d');
 
 
 
 function drawbars(nums) {
-  c.fillStyle = '#3498DB'
+  c.clearRect(0, 0, window.innerWidth, window.innerHeight)
   const y = 500
   var x = 100
   for (var i = 0; i < nums.length; i += 1) {
-    c.fillRect(x, y - nums[i].value, 10, nums[i].value)
+    c.fillStyle = nums[i].color
+    c.fillRect(x, y - nums[i].value, 5, nums[i].value)
     x += 10
   }
-  console.log('drawn all')
 
 
 }
--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 function bar() {
   const min = 10
-  const max = 100
+  const max = 200
   return {
     value: Math.floor(Math.random() * (max - min))  + min,
-    color: "#3498DB"
+    color: "#F4D03F"
   }
 }
 const nums = []
 
 generateArray()
-console.log(nums)
 
 function generateArray() {
   for (var i = 0; i < 100; i++) {
@@ -18,3 +17,35 @@ function generateArray() {
 }
 
 drawbars(nums)
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+async function insertionSort(arr) {
+  for (var i = 0; i < arr.length; i += 1) {
+    var min = arr[i].value
+    var min_index = i
+
+    for(var j = i; j < arr.length; j += 1) {
+      if (arr[j].value < arr[min_index].value) {
+        min = arr[j].value
+        min_index = j
+      }
+    }
+    temp = arr[i].value
+    arr[i].value = arr[min_index].value
+    arr[min_index].value = temp
+    drawbars(arr)
+    await sleep(50);
+  }
+
+}
+
+
+
+function sort () {
+  insertionSort(nums)
+}
