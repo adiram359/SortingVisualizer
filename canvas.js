08119f9e881cb1591b226fd953b1628905335a6b
Aditya Ramaakrushnan <48169134+adiram359@users.noreply.github.com>
2020-06-13 10:05:56 -0700

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -2,9 +2,45 @@ const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const sortButton = document.querySelector('.algobutton')
-console.log(sortButton)
-sortButton.addEventListener('click', () => sort())
+document.querySelector('.algobutton1').addEventListener('click', form1submission)
+
+document.querySelector('.algobutton2').addEventListener('click', form2submission)
+
+document.querySelector('.algobutton3').addEventListener('click', form3submission)
+
+
+
+
+function form1submission(event) {
+  event.preventDefault()
+  var block = document.getElementById("001");
+  var option = block.options[block.selectedIndex].value
+  const details = {block: 1,
+                    option:option}
+  console.log(details)
+  sort(details)
+}
+
+function form2submission(event) {
+  event.preventDefault()
+  var block = document.getElementById("002");
+  var option = block.options[block.selectedIndex].value
+  const details = {block: 2,
+                    option:option}
+  console.log(details)
+  sort(details)
+}
+
+function form3submission(event) {
+  event.preventDefault()
+  var block = document.getElementById("003");
+  var option = block.options[block.selectedIndex].value
+  const details = {block: 3,
+                    option:option}
+  console.log(details)
+  sort(details)
+}
+
 
 
 
@@ -14,13 +50,14 @@ var c = canvas.getContext('2d');
 
 function drawbars(nums) {
   c.clearRect(0, 0, window.innerWidth, window.innerHeight)
-  const y = 500
-  var x = 100
+  const y = 240
+  var x = 240
   for (var i = 0; i < nums.length; i += 1) {
     c.fillStyle = nums[i].color
     c.fillRect(x, y - nums[i].value, 5, nums[i].value)
-    x += 10
+    x += 7
   }
+
 
 
 }
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function bar() {
   const min = 10
-  const max = 200
+  const max = 160
   return {
     value: Math.floor(Math.random() * (max - min))  + min,
     color: "#F4D03F"
@@ -41,11 +41,94 @@ async function insertionSort(arr) {
     drawbars(arr)
     await sleep(50);
   }
+}
+async function bubbleSort(arr) {
+  for (var i = 0; i < arr.length ; i += 1) {
+    for(var j = i; j > 0; j -= 1) {
+      if (arr[j].value < arr[j - 1].value) {
+        const temp = arr[j - 1].value
+        arr[j - 1].value = arr[j].value
+        arr[j].value = temp
+      }
+    }
+    drawbars(arr)
+    await sleep(50);
+  }
+}
+
+async function mergeSort(arr, start, end) {
+
+  // await drawbars(nums)
+  // await sleep(20)
+
+  if (start >= end) {
+    return;
+  }
+  const middle = Math.floor((start + end) / 2)
+
+
+  await mergeSort(arr, start, middle)
+  await mergeSort(arr, middle + 1, end)
+
+  const length1 = middle - start + 1;
+  const length2 = end - middle;
+  const L= new Array(length1);
+  const R = new Array(length2);
+  for (var i = 0; i < length1; i += 1)
+      L[i] = {value: arr[start + i].value};
+  for (var j = 0; j < length2; j += 1)
+      R[j]= {value: arr[middle + j + 1].value};
+  var i = 0
+  var j = 0
+  var k = start;
+  while (i < length1 && j < length2) {
+      if (L[i].value <= R[j].value) {
+          arr[k].value = L[i].value;
+          i += 1;
+      }
+      else {
+          arr[k].value = R[j].value;
+          j++;
+      }
+      k += 1;
+  }
+  while (i < length1) {
+      arr[k].value = L[i].value;
+      i += 1;
+      k += 1;
+  }
+  while (j < length2) {
+      arr[k].value = R[j].value;
+      j += 1;
+      k += 1;
+  }
+  await drawbars(nums)
+  await sleep(50)
 
 }
 
 
+async function sort (details) {
+  if (details.option === "Insertion Sort") {
+    insertionSort(nums)
+  }
+  else if (details.option === "Bubble Sort") {
+    bubbleSort(nums)
+  }
+  else if (details.option === "Merge Sort") {
+    console.log(nums.map((ob) => ob.value))
+    await mergeSort(nums, 0, nums.length - 1)
+    console.log(nums.map((ob) => ob.value))
+  }
+}
 
-function sort () {
-  insertionSort(nums)
+
+async function countdown(num) {
+  if (num === 0) {
+    console.log(num)
+    return;
+  }
+  await sleep(1000)
+  console.log(num)
+  countdown(num - 1)
 }
